04ac1081dc17827d114d2c2f0ecc686062aa59c8
alexisconstantino-rgb <alexis.constantino@uabc.edu.mx>
2026-02-06 23:56:59 +0000

    save

--- rzecuad.c
+++ rzecuad.c
@@ -4,8 +4,10 @@
 int main (void){
   float la, lb, lc;
   printf("El actual programa permitira calcular las raices de una ecuacion cuadratica.\n");
-  /*printf(); scanf();
-  printf(); scanf();
-  printf(); scanf();*/
+  printf("Una ecuacion cuadratica tiene la forma: ax^2 + bx + c\n");
+  /*printf("Escribe el valor de a:\n"); scanf(" %f", &la);
+  printf("Escribe el valor de b:\n"); scanf(" %f", &lb);
+  printf("Escribe el valor de c:\n"); scanf(" %f", &lc);*/
+  //formula = x1/x2 = (-b +- (b**2 - 4ac)**0.5)/(2a)
   return 0;
 }
