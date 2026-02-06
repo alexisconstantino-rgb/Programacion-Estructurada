0a9c7defba25b2b48355dcc0b2a1aeb6f8c125c0
alexisconstantino-rgb <alexis.constantino@uabc.edu.mx>
2026-02-06 15:28:28 -0800

    Create raizescuadraticas.c
    Creacion del archivo.c que calculara las raices de una Ec. Cuadratica.

---
+++ raizescuadraticas.c
@@ -0,0 +1,11 @@
+//programa para calcular las raices de una ecuacion cuadratica
+#include <stdio.h>
+
+int main (void){
+  float la, lb, lc;
+  printf("El actual programa permitira calcular las raices de una ecuacion cuadratica.\n");
+  /*printf(); scanf();
+  printf(); scanf();
+  printf(); scanf();*/
+  return 0;
+}
