c5f8124ecb0b7d1fcb7c92e1f7389cc9f5fdf8a0
alexisconstantino-rgb <alexis.constantino@uabc.edu.mx>
2026-03-09 20:16:53 +0000

    End: practica5

--- practica5/practica5.c
+++ practica5/practica5.c
@@ -1,84 +1,121 @@
 #include <stdio.h>
-//def's
-//#define ESTUDIANTES 100
 
 //prototipos
-
+int numEstudiantes(void);
+void registrarCalificaciones(float calificaciones[], int estudiantes);
+float calpromedio (float calificaciones[], int estudiantes);
+float calificacionalta(float calificaciones[], int estudiantes);
+float calificacionbaja(float calificaciones[], int estudiantes);
+void resultados(float promedio, float calfalta, float calfbaja, int aprobados, int reprobados);
+int aprobados (float calificaciones[], int estudiantes);
+int reprobados (float calificaciones[], int estudiantes);
+float validar(float min, float max);
+void cleanbbf (void);
 //main
 int main(void)
 {
-	float estudiantes;
+	int estudiantes;
 	float promedio, calfalta, calfbaja;
-	float estudiantesap, estududiantesrep;
+	int estudiantesap, estudiantesrep;
 	printf("Presentacion: \n");
 	//pedir al usuario la cantidad de estudiantes
 	estudiantes = numEstudiantes();
-	float calficaciones[estudiantes];
-	//pedir la calificacion final de cada estudiante
-	registarCalificaciones(calificaciones, estudiantes);
-	//calcular estadisticas
-
-	//mostra resultados de: Promedio grupal, Cal mas alta y baja, Num de estudiantes aprobados y reprobados.
-	
+	if(estudiantes == 0)
+		printf("Sin estudiantes no hay resultados.\n");
+	else
+	{
+		float calificaciones[estudiantes];
+		//pedir la calificacion final de cada estudiante
+		registrarCalificaciones(calificaciones, estudiantes);
+		//calcular estadisticas
+		promedio = calpromedio(calificaciones, estudiantes);
+		calfalta = calificacionalta(calificaciones, estudiantes);
+		calfbaja = calificacionbaja(calificaciones, estudiantes);
+		estudiantesap = aprobados(calificaciones, estudiantes);
+		estudiantesrep = reprobados(calificaciones, estudiantes);
+		//mostra: Promedio grupal, Cal mas alta y baja, Num estudiantes aprobados y reprobados.
+		resultados (promedio, calfalta, calfbaja, estudiantesap, estudiantesrep);	
+	}
 	return 0;
 }
 /*TAREA 1*/
 //Leer numero de estudiantes
-float numEstudiantes(void)
+int numEstudiantes(void)
 {
-	float estudiantes;
-	printf("Cantidad de alumnos presentes en el grupo:\t");
-	estudiantes = validar(1, 100);
+	int estudiantes;
+	printf("Cantidad de alumnos presentes en el grupo\n");
+	estudiantes = validar(0, 100);
 	return estudiantes;
 }
 /*TAREA 2*/
 //Registrar Calificaciones
-void registrarCalificaciones(float calificaciones[], float estudiantes)
+void registrarCalificaciones(float calificaciones[], int estudiantes)
 {
-	for (int i=0; i <= estudiantes; i++)
+	for (int i=0; i < estudiantes; i++)
 	{
-		printf("Ingrese la calificacion del %d^o en la lista de alumnos.\n", i+1 );
+		printf("Calificacion del %d^o en la lista de alumnos:\n", i+1 );
 		calificaciones[i] = validar(0, 100);
+		printf("\n");
 	}
 	printf("Registro Copleto\n\n");
 }
 /*TAREA 3*/
 //Promedio del grupo
-float promedio (float calificaciones[], float estudiantes)
+float calpromedio (float calificaciones[], int estudiantes)
 {
 	float output=0;
-	for(int i=0; i <= estudiantes; i++)
+	for(int i=0; i < estudiantes; i++)
 	{
 		output += calificaciones[i];
 	}
 	return (output / estudiantes);
 }
 //Calificacion mas alta
-float calificacionalta(float calificaciones[], float estudiantes)
+float calificacionalta(float calificaciones[], int estudiantes)
 {
-	float alta=0;
-	for(int i=0; i<=estudiantes; i++)
+	float alta=calificaciones[0];
+	for(int i=0; i<estudiantes; i++)
 	{
 		if(calificaciones[i]>alta)
-			alta = calificacion[i];
+			alta = calificaciones[i];
 	}
+	return alta;
 }
 //Calificacion mas baja
-float calificacionbaja(float calificaciones[], float estudiantes)
+float calificacionbaja(float calificaciones[], int estudiantes)
 {
-	float baja=0;
-	for(int i=0; i<=estudiantes; i++)
+	float baja=calificaciones[0];
+	for(int i=0; i<estudiantes; i++)
 	{
-		if(calificaciones[i]>alta)
-			baja = calificacion[i];
+		if(calificaciones[i] < baja)
+			baja = calificaciones[i];
 	}
+	return baja;
 }
 //Numero de estudiantes aprobados (>=60)
-
+int aprobados(float calificaciones[], int estudiantes)
+{
+	float count=0;;
+	for(int i=0; i<estudiantes; i++)
+	{
+		if(calificaciones[i] >= 60)
+			count ++;
+	}
+	return count;
+}	
 //Numero de estudiantes reprobados (<= 60)
-
+int reprobados(float calificaciones[], int estudiantes)
+{
+	float count=0;;
+	for(int i=0; i<estudiantes; i++)
+	{
+		if(calificaciones[i] < 60)
+			count ++;
+	}
+	return count;
+}	
 /*TAREA 4: MOSTAR RESULTADOS*/
-void resultados ()
+void resultados (float promedio, float calfalta, float calfbaja, int aprobados, int reprobados)
 {
 	int opc;
 	do{
@@ -87,25 +124,37 @@ void resultados ()
 		scanf(" %d", &opc);
 		switch(opc)
 		{
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
+			case 1:	printf("Promedio grupal: %.2f\n", promedio);
+			       	break;
+			case 2: printf("Calficacion mas alta: %.2f\n", calfalta);
+				printf("Calificacion mas baja: %.2f\n", calfbaja);
+				break;
+			case 3: printf("Cantidad de aprobados: %d\n", aprobados);
+				printf("Cantidad de reprobados: %d\n", reprobados);
+				break;
+			case 4: printf("Saliendo..\n"); break;
+			default: printf("Opcion Invalida, try again.\n");
 		}
-
+	printf("\n");
 	}while(opc != 4);
 }
-
 /*SUBTAREAS*/
 float validar (float min, float max)
 {	
 	float input=0;
-	float aux=0;
+	int aux=0;
 	do{
-		printf("valor (%.1f - %.1f):\t", min, max)
-		aux = scanf(" %f", &input)
-		if(input <= min || input >= max || aux != 1.0)
+		printf("El valor debe estar entre %.1f - %.1f :\t", min, max);
+		aux = scanf(" %f", &input);
+		if(input < min || input > max || aux != 1){
 			printf("Syntax error, try again\n");
-	}while(input <= min || input >= max || aux != 1.0);
+			cleanbbf();
+		}
+	}while(input < min || input > max || aux != 1);
 	return input;
+}
+void cleanbbf (void)
+{
+	int c;
+	while((c = getchar()) != '\n' && c != EOF);
 }
